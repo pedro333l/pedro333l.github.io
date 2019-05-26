43bcde3554ea811429d649684f2a2b889c54473e
pedro333l <50372497+pedro333l@users.noreply.github.com>
2019-05-25 22:45:35 -0300

    Add files via upload

---
+++ ggg/offline.js
@@ -0,0 +1,1 @@
+{"version":1558834982726,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/shared-0-sheet2.png","icons/icon-16.png","icons/icon-32.png","icons/icon-114.png","icons/icon-128.png","icons/icon-256.png","icons/loading-logo.png","register-sw.js"]}
