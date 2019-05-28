3d674a6b57ca27403160870b09ba12cc85111ffe
pedro333l <50372497+pedro333l@users.noreply.github.com>
2019-05-28 18:40:39 -0300

    Add files via upload

---
+++ wp/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1559079491,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/amigo-sheet0.png",
+		"images/botaodeplya-sheet0.png",
+		"images/peiamigo-sheet0.png",
+		"images/cima-sheet0.png",
+		"images/lado1-sheet0.png",
+		"images/lado2-sheet0.png",
+		"images/baixo-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/peuinimigo-sheet0.png",
+		"images/hp-sheet0.png",
+		"images/hp3-sheet0.png",
+		"images/valorhp.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
